c890931207158e2af26bbadf7f96838947e141bd
RG Aquino <ralphgregoraquino@gmail.com>
2019-04-08 22:01:56 +0800

    Update dependencies

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class App extends React.Component {
+class App extends Component {
   render() {
-    return (<div>
+    return (
+    <div>
       <h2>Welcome to React!</h2>
     </div>);
   }
 }
+
+export default App;
