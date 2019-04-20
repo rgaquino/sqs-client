31230f20a4395ef7001c88466d6d5bf0a7d74810
RG Aquino <ralphgregoraquino@gmail.com>
2019-04-20 14:50:14 +0800

    SQS-0001: Integrate controller to components

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-
-import store from './store';
+import QueueList from './app/components/queueList';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <div>
-          <h2>Welcome to React!</h2>
-        </div>
-      </Provider>
+      <QueueList />
     );
   }
 }
---
+++ src/renderer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './app';
+
+const render = () => {
+  ReactDOM.render(<AppContainer><App /></AppContainer>, document.getElementById('app'));
+};
+
+render();
+if (module.hot) {
+  module.hot.accept(render);
+}
