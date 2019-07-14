479f20bd9b1216cda34a41bcc29a6eea239333df
RG Aquino <ralphgregoraquino@gmail.com>
2019-07-14 20:54:23 +0800

    SQS-0022: Remove link in configuration header

--- src/app/components/configuration/configuration.jsx
+++ src/app/components/configuration/configuration.jsx
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { getConnectionConfig, setConnectionConfig } from '../../actions/configActions';
 
